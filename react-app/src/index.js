5a7728284f811e13a1838b1784796290f2c69c6e
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2019-10-14 13:29:51 +0700

    added react routing templates

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,5 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import NavBar from './components/NavBar';
 
-ReactDOM.render(<NavBar />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/users">Users</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+                <Route path="/about">
+                    <h1>About</h1>
+                </Route>
+                <Route path="/users">
+                    <h1>Users</h1>
+                </Route>
+                <Route path="/">
+                    <h1>Home</h1>
+                </Route>
+            </Switch>
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
