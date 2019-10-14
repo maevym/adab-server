d6d55344c5d07f81929437bdc9cf86c361694a62
Stanley <angstanley68@gmail.com>
2019-10-15 00:34:12 +0700

    Lojin

--- ServerApp/controller.js
+++ ServerApp/controller.js
@@ -5,3 +5,13 @@ const db = require('./conn');
 const md5 = require('md5');
 const crypto = require('crypto');
 const cookie = require('cookie');
+const axios = require('axios');
+
+exports.login = (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+};
+
+exports.doLogin = (req, res) => {
+    const {username, password} = req.body;
+
+};
--- ServerApp/routes.js
+++ ServerApp/routes.js
@@ -13,6 +13,9 @@ module.exports = function(app) {
         res.sendFile(__dirname + '/public/home.html');
     });
 
+    app.route('/login')
+        .get(controller.login);
+
     // API Routes below
     app.route('/api/v1/login')
         .post(todoList.login);
