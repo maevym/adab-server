0b577a308b33a9235e0e9f5b479846c5efbcdb3c
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2019-10-14 12:52:44 +0700

    created temporary navbar component

---
+++ react-app/src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './css/NavBar.css';
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <p>Icon</p>
+                <input className="search-input"/>
+            </div>
+        );
+    }
+}
+
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
+
+export default NavBar;
