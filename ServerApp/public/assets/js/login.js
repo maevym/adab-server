c17aedceaa36d179a9e06ce1ac35ad405353ec65
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2019-10-15 01:09:38 +0700

    NaN 2.1

--- ServerApp/public/assets/js/login.js
+++ ServerApp/public/assets/js/login.js
@@ -1,8 +1,1 @@
-const url = '/api/v1';
-
-const loginButton = document.querySelector('#btn-login');
-const loginForm = document.querySelector('#login-form');
-
-loginButton.onclick = () => {
-    loginForm.submit();
-};
+// empty js file
