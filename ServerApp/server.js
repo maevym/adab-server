71eb35973f512589b49cd899a65ee19f91d3b3f5
Stanley <angstanley68@gmail.com>
2019-10-14 21:14:00 +0700

    Update SQL

--- ServerApp/server.js
+++ ServerApp/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const controller = require('./controller');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
