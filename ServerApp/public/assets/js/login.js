8be1fdbf18999aee6ef948936c76d7b6af391d1e
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2019-10-15 01:17:20 +0700

    NaN 3.0

--- ServerApp/public/assets/js/login.js
+++ ServerApp/public/assets/js/login.js
@@ -1,1 +1,10 @@
-// empty js file
+const urlParams = new URLSearchParams(window.location.search);
+const errorMsg = document.querySelector('#error-msg');
+
+if (urlParams.has('error')) {
+    errorMsg.hidden = false;
+    errorMsg.innerText = urlParams.get('error');
+} else {
+    errorMsg.hidden = true;
+    errorMsg.innerText = '';
+}
