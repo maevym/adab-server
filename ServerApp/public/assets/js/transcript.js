8ee77d0e0c197b46a828cdc4be39057df0836101
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2019-10-15 09:55:47 +0700

    hm guan na

--- ServerApp/public/assets/js/transcript.js
+++ ServerApp/public/assets/js/transcript.js
@@ -1,18 +1,27 @@
 const transcript = document.getElementById("transcript");
+const startTalk = document.getElementById("start-talking");
+let huamamamamamama = true;
+
 function recognize() {
     const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-    recognition.lang = 'id-ID';
-    recognition.interimResults = false;
-    recognition.onresult = function(event) {
-        console.log('You said: ', event.results[0][0].transcript);
-        transcript.innerHTML += (event.results[0][0].transcript);
-    };
-    recognition.onerror = function (evt) {
-        console.log(evt);
-    };
-    recognition.onend = function(){
-        recognition.stop();
+    if (huamamamamamama) {
+        startTalk.innerText = 'Stop Talking';
+        recognition.lang = 'id-ID';
+        recognition.interimResults = false;
+        recognition.onresult = function (event) {
+            console.log('You said: ', event.results[0][0].transcript);
+            transcript.innerHTML += (event.results[0][0].transcript);
+        };
+        recognition.onerror = function (evt) {
+            console.log(evt);
+        };
+        recognition.onend = function () {
+            recognition.stop();
+            recognition.start();
+        };
         recognition.start();
-    };
-    recognition.start();
+    } else {
+        startTalk.innerText = 'Stop Talking';
+        recognition.stop();
+    }
 }
