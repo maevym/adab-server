f4000806d853f87d578c7c352b6134253b87f9be
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2019-10-15 10:11:32 +0700

    lai liau le

--- ServerApp/public/assets/js/transcript.js
+++ ServerApp/public/assets/js/transcript.js
@@ -1,27 +1,19 @@
 const transcript = document.getElementById("transcript");
-const startTalk = document.getElementById("start-talking");
-let huamamamamamama = true;
 
 function recognize() {
     const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-    if (huamamamamamama) {
-        startTalk.innerText = 'Stop Talking';
-        recognition.lang = 'id-ID';
-        recognition.interimResults = false;
-        recognition.onresult = function (event) {
-            console.log('You said: ', event.results[0][0].transcript);
-            transcript.innerHTML += (event.results[0][0].transcript);
-        };
-        recognition.onerror = function (evt) {
-            console.log(evt);
-        };
-        recognition.onend = function () {
-            recognition.stop();
-            recognition.start();
-        };
-        recognition.start();
-    } else {
-        startTalk.innerText = 'Stop Talking';
+    recognition.lang = 'id-ID';
+    recognition.interimResults = false;
+    recognition.onresult = function(event) {
+        console.log('You said: ', event.results[0][0].transcript);
+        transcript.innerHTML += (event.results[0][0].transcript);
+    };
+    recognition.onerror = function (evt) {
+        console.log(evt);
+    };
+    recognition.onend = function(){
         recognition.stop();
-    }
+        recognition.start();
+    };
+    recognition.start();
 }
