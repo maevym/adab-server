ee299abbc8fde7154ea6d0b0001f0c80006df664
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2019-10-15 10:16:05 +0700

    ok ni na ma gu hu tu ya gun ba

--- ServerApp/public/assets/js/transcript.js
+++ ServerApp/public/assets/js/transcript.js
@@ -1,19 +1,28 @@
 const transcript = document.getElementById("transcript");
+const startBicara = document.getElementById("transcript");
+let nenglanghunjukbanghu = true;
+let recognition;
 
 function recognize() {
-    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-    recognition.lang = 'id-ID';
-    recognition.interimResults = false;
-    recognition.onresult = function(event) {
-        console.log('You said: ', event.results[0][0].transcript);
-        transcript.innerHTML += (event.results[0][0].transcript);
-    };
-    recognition.onerror = function (evt) {
-        console.log(evt);
-    };
-    recognition.onend = function(){
-        recognition.stop();
+    if (nenglanghunjukbanghu) {
+        startBicara.innerText = 'Stop Talking';
+        recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+        recognition.lang = 'id-ID';
+        recognition.interimResults = false;
+        recognition.onresult = function (event) {
+            console.log('You said: ', event.results[0][0].transcript);
+            transcript.innerHTML += (event.results[0][0].transcript);
+        };
+        recognition.onerror = function (evt) {
+            console.log(evt);
+        };
+        recognition.onend = function () {
+            recognition.stop();
+            recognition.start();
+        };
         recognition.start();
-    };
-    recognition.start();
+    } else {
+        startBicara.innerText = 'Start Talking';
+        recognition.stop();
+    }
 }
