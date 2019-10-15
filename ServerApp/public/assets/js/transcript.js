e56b22812afc6c79f83a61025dcf7d901081a2e1
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2019-10-15 10:33:07 +0700

    xiao zhi yi gu huan

--- ServerApp/public/assets/js/transcript.js
+++ ServerApp/public/assets/js/transcript.js
@@ -3,6 +3,14 @@ const startBicara = document.getElementById("start-talking");
 let nenglanghunjukbanghu = true;
 let recognition;
 
+const urlParamsssssss = new URLSearchParams(window.location.search);
+const socket = io();
+socket.emit('join_room', urlParamsssssss.get('id'));
+
+socket.on('message', (m) => {
+    transcript.innerHTML += " " + m;
+});
+
 function recognize() {
     if (nenglanghunjukbanghu) {
         startBicara.innerText = 'Stop Talking';
@@ -11,7 +19,7 @@ function recognize() {
         recognition.interimResults = false;
         recognition.onresult = function (event) {
             console.log('You said: ', event.results[0][0].transcript);
-            transcript.innerHTML += " " + (event.results[0][0].transcript);
+            socket.emit('message', event.results[0][0].transcript);
         };
         recognition.onerror = function (evt) {
             console.log(evt);
