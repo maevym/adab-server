182fc4ce28ae97411986a4060dce47d58ec3be34
Stanley <angstanley68@gmail.com>
2019-10-17 12:27:13 +0700

    Update api.js
    Allow authorization from header

--- ServerApp/api.js
+++ ServerApp/api.js
@@ -7,11 +7,12 @@ const crypto = require('crypto');
 const cookie = require('cookie');
 
 exports.newDiscussion = (req, res) => {
-    const cookies = cookie.parse(req.headers.cookie || '');
     const {session_id: sessionId, reply_to: replyTo, content} = req.body;
-    const {session_id: userToken} = cookies;
+    const auth = req.get("authorization");
+    const credentials = auth.split(" ").pop();
+    const userToken = credentials || cookie.parse(req.headers.cookie || '').session_id;
 
-    if (cookies == null || cookies.session_id == null) {
+    if (userToken == null) {
         response.unauthorized("Unauthorized", res);
         return;
     }
@@ -37,12 +38,13 @@ exports.newDiscussion = (req, res) => {
 exports.discussions = (req, res) => {
     const query = `SELECT discussion_id, author_id, author.user_name AS author_name, content, reply_to, timestamp FROM discussions INNER JOIN user_sessions ON user_sessions.session_id = discussions.session_id INNER JOIN users AS author ON discussions.author_id = author.user_id INNER JOIN users AS mhs ON user_sessions.user_id = mhs.user_id WHERE discussions.session_id = ? AND mhs.user_token = ?`;
 
-    const cookies = cookie.parse(req.headers.cookie || '');
+    const auth = req.get("authorization");
+    const credentials = auth.split(" ").pop();
+    const userToken = credentials || cookie.parse(req.headers.cookie || '').session_id;
 
-    if (cookies == null || cookies.session_id == null) {
+    if (userToken == null) {
         response.unauthorized("Unauthorized", res);
     } else {
-        const {session_id: userToken} = cookies;
         const {session_id: sessionId} = req.body;
         db.all(query, [sessionId, userToken], (error, data) => {
             if (!error) {
@@ -58,12 +60,13 @@ exports.discussions = (req, res) => {
 exports.sessionDetails = (req, res) => {
     const query = "SELECT user_sessions.session_id, courses.course_id, course_name, course_description, topic_title, topic_description, session_th, session_mode, classes.class_name, session_campus, session_room, dosen.user_id AS lecturer_id, dosen.user_name AS lecturer_name, session_startdate, session_enddate, can_talk, content FROM sessions INNER JOIN classes ON sessions.class_id = classes.class_id INNER JOIN courses ON classes.course_id = courses.course_id INNER JOIN user_sessions ON sessions.session_id = user_sessions.session_id INNER JOIN users AS dosen ON classes.class_lecturer_id = dosen.user_id INNER JOIN users AS mahasiswa ON user_sessions.user_id = mahasiswa.user_id INNER JOIN user_groups ON user_groups.group_id = mahasiswa.user_group WHERE sessions.session_id = ? AND mahasiswa.user_token = ?";
 
-    const cookies = cookie.parse(req.headers.cookie || '');
+    const auth = req.get("authorization");
+    const credentials = auth.split(" ").pop();
+    const userToken = credentials || cookie.parse(req.headers.cookie || '').session_id;
 
-    if (cookies == null || cookies.session_id == null) {
+    if (userToken == null) {
         response.unauthorized("Unauthorized", res);
     } else {
-        const {session_id: userToken} = cookies;
         const {session_id: sessionId} = req.body;
         db.get(query, [sessionId, userToken], (error, row) => {
             if (!error) {
@@ -83,12 +86,13 @@ exports.sessionDetails = (req, res) => {
 exports.getSessions = (req, res) => {
     const query = "SELECT sessions.session_id, courses.course_id, course_name, session_th, session_mode, classes.class_name, topic_title, topic_description, session_campus, session_room, users.user_id AS lecturer_id, users.user_name AS lecturer_name, session_startdate, session_enddate FROM user_sessions INNER JOIN sessions ON user_sessions.session_id = sessions.session_id INNER JOIN classes ON sessions.class_id = classes.class_id INNER JOIN courses ON classes.course_id = courses.course_id INNER JOIN users ON classes.class_lecturer_id = users.user_id  WHERE user_sessions.user_id = (SELECT user_id FROM users WHERE user_token = ?)";
 
-    const cookies = cookie.parse(req.headers.cookie || '');
+    const auth = req.get("authorization");
+    const credentials = auth.split(" ").pop();
+    const userToken = credentials || cookie.parse(req.headers.cookie || '').session_id;
 
-    if (cookies == null || cookies.session_id == null) {
+    if (userToken == null) {
         response.unauthorized("Unauthorized", res);
     } else {
-        const {session_id: userToken} = cookies;
         db.all(query, [userToken], (error, row) => {
             if (!error) {
                 if (row.length > 0) {
@@ -105,7 +109,9 @@ exports.getSessions = (req, res) => {
 
 exports.getProfilePicture = (req, res) => {
     const query = "SELECT * FROM users WHERE user_token = ?";
-    const {session_id: userToken} = cookie.parse(req.headers.cookie || '');
+    const auth = req.get("authorization");
+    const credentials = auth.split(" ").pop();
+    const userToken = credentials || cookie.parse(req.headers.cookie || '').session_id;
 
     db.get(query, [userToken], (error, row) => {
        if (!error) {
@@ -128,7 +134,9 @@ exports.getProfilePicture = (req, res) => {
 
 exports.profile = (req, res) => {
     const query = "SELECT user_id, user_name, user_email, group_name, is_staff, can_talk FROM users INNER JOIN user_groups ON users.user_group = user_groups.group_id WHERE user_token = ?";
-    const {session_id: userToken} = cookie.parse(req.headers.cookie || '');
+    const auth = req.get("authorization");
+    const credentials = auth.split(" ").pop();
+    const userToken = credentials || cookie.parse(req.headers.cookie || '').session_id;
 
     db.get(query, [userToken], (error, row) => {
        if (!error) {
