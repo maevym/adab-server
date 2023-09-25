f1a2846054ed8169429b34b9ec284e8c3115d010
Maevy <maevym70@gmail.com>
2023-09-25 23:32:04 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -6,73 +6,76 @@ module.exports = function(app) {
     const checkAdminToken = require('./middleware/checkAdminToken');
 
     // HTML routes below
-    app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/public/index.html');
-    });
+    // app.get('/', (req, res) => {
+    //     res.sendFile(__dirname + '/public/index.html');
+    // });
 
-    app.route('/login')
-        .get(controller.login);
+    // app.route('/login')
+    //     .get(controller.login);
 
-    app.route('/login')
-        .post(controller.doLogin);
+    // app.route('/login')
+    //     .post(controller.doLogin);
 
-    app.route('/home')
-        .get(controller.home);
+    // app.route('/home')
+    //     .get(controller.home);
 
-    app.route('/session')
-        .get(controller.session);
+    // app.route('/session')
+    //     .get(controller.session);
 
-    app.get('/socket', (req, res) => {
-        res.sendFile(__dirname + '/public/socket.html');
-    });
+    // app.get('/socket', (req, res) => {
+    //     res.sendFile(__dirname + '/public/socket.html');
+    // });
 
     // API Routes below
     app.route('/api/v1/login')
         .post(todoList.login);
 
-    app.route('/api/v1/user/profile')
-        .get(todoList.profile);
+    app.route('/api/v1/register')
+        .post(todoList.register);
 
-    app.route('/api/v1/user/picture')
-        .get(todoList.getProfilePicture);
+    // app.route('/api/v1/user/profile')
+    //     .get(todoList.profile);
 
-    app.route('/api/v1/user/sessions')
-        .get(todoList.getSessions);
+    // app.route('/api/v1/user/picture')
+    //     .get(todoList.getProfilePicture);
 
-    app.route('/api/v1/session/details')
-        .post(todoList.sessionDetails);
+    // app.route('/api/v1/user/sessions')
+    //     .get(todoList.getSessions);
 
-    app.route('/api/v1/session/discussions')
-        .post(todoList.discussions);
+    // app.route('/api/v1/session/details')
+    //     .post(todoList.sessionDetails);
 
-    app.route('/api/v1/session/discussions/new')
-        .post(todoList.newDiscussion);
+    // app.route('/api/v1/session/discussions')
+    //     .post(todoList.discussions);
+
+    // app.route('/api/v1/session/discussions/new')
+    //     .post(todoList.newDiscussion);
 
     //API Routes for Admin Web
-    app.route('/api/v1/admin/retrieveAllUsers')
-        .get(checkAdminToken,todoList.retrieveAllUsers);
+    // app.route('/api/v1/admin/retrieveAllUsers')
+    //     .get(checkAdminToken,todoList.retrieveAllUsers);
 
-    app.route('/api/v1/admin/retrieveAllCourses')
-        .get(checkAdminToken,todoList.retrieveAllCourses);
+    // app.route('/api/v1/admin/retrieveAllCourses')
+    //     .get(checkAdminToken,todoList.retrieveAllCourses);
 
-    app.route('/api/v1/admin/retrieveAllSessions')
-        .get(checkAdminToken,todoList.retrieveAllSessions);
+    // app.route('/api/v1/admin/retrieveAllSessions')
+    //     .get(checkAdminToken,todoList.retrieveAllSessions);
 
-    app.route('/api/v1/admin/createNewUser')
-        .post(checkAdminToken,todoList.createNewUser);
+    // app.route('/api/v1/admin/createNewUser')
+    //     .post(checkAdminToken,todoList.createNewUser);
 
-    app.route('/api/v1/admin/createNewCourse')
-        .post(checkAdminToken,todoList.createNewCourse);
+    // app.route('/api/v1/admin/createNewCourse')
+    //     .post(checkAdminToken,todoList.createNewCourse);
 
-    app.route('/api/v1/admin/createNewSession')
-        .post(checkAdminToken,todoList.createNewSession);
+    // app.route('/api/v1/admin/createNewSession')
+    //     .post(checkAdminToken,todoList.createNewSession);
         
-    app.route('/api/v1/admin/updateExistingUser/:user_id')
-        .patch(checkAdminToken,todoList.updateExistingUser);  
+    // app.route('/api/v1/admin/updateExistingUser/:user_id')
+    //     .patch(checkAdminToken,todoList.updateExistingUser);  
 
-    app.route('/api/v1/admin/updateExistingCourse/:courseId')
-        .patch(checkAdminToken,todoList.updateExistingCourse);
+    // app.route('/api/v1/admin/updateExistingCourse/:courseId')
+    //     .patch(checkAdminToken,todoList.updateExistingCourse);
 
-    app.route('/api/v1/admin/updateExistingSession/:sessionId')
-        .patch(checkAdminToken,todoList.updateExistingSession);    
+    // app.route('/api/v1/admin/updateExistingSession/:sessionId')
+    //     .patch(checkAdminToken,todoList.updateExistingSession);    
 };
